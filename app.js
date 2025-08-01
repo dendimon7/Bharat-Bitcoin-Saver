e6a5115459a41a8d0adb6435154873e87b5de2e3
dendimon7 <135828999+dendimon7@users.noreply.github.com>
2025-08-01 20:23:56 +0530

    Updated app.js to reflect the new app name: Bharat Sats Stacker

---
+++ app.js
@@ -0,0 +1,15 @@
+// Welcome to Bharat Sats Stacker
+console.log('Starting Bharat Sats Stacker...');
+
+function startApp() {
+    console.log('Bharat Sats Stacker is running!');
+}
+
+startApp();
+
+// This is the main function for Bharat Sats Stacker
+function main() {
+    console.log('Main function of Bharat Sats Stacker');
+}
+
+main();
